75c9c7cdf23ffde100d4e5e5698386cd39306bfb
Xoto751 <xotobeyn8970111@gmail.com>
2025-01-29 23:48:34 +0300

    Update Lab_6_Task_2_Library.py
    Исправлено, теперь выбирает следующий id по возрастанию в библиотеке, а не следующий по порядку

--- Лабораторная работа №6/Lab_6_Task_2_Library.py
+++ Лабораторная работа №6/Lab_6_Task_2_Library.py
@@ -36,7 +36,8 @@ class Library:
     def get_next_book_id(self) -> int:
         if not self.books:
             return 1
-        return self.books[-1].id + 1
+        max_id = max(book.id for book in self.books)
+        return max_id + 1
 
     def get_index_by_book_id(self, book_id: int) -> int:
         for index, book in enumerate(self.books):
